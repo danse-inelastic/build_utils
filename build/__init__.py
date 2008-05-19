6b750aa6ffada27fe5e0e9a0720da45da9da39fd
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2008-05-19 22:13:07 +0000

    fixed a bug
    git-svn-id: svn://danse.us/buildInelast/common/utils@262 e9de5ee5-d905-4afb-841f-18304ef63348

--- build/__init__.py
+++ build/__init__.py
@@ -70,7 +70,6 @@ def _build(release, builddirs, args = []):
                 arguments = args)
             succeeded = True
         except build.DependencyMissing, dep:
-            raise
             print "Trying to install dependency '%s' ..." % dep
             from deps import install
             install( str(dep) )
