60036e427b5209e513ffb42dd54154636084dfd6
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2008-05-21 21:47:24 +0000

    utils/installers/download.py: download_using_svn needs more attention: use a temporary directory
    git-svn-id: svn://danse.us/buildInelast/common/utils@271 e9de5ee5-d905-4afb-841f-18304ef63348

--- installers/download.py
+++ installers/download.py
@@ -21,20 +21,29 @@ def download_cmd( link ):
         raise NotImplementedError
 
 def download_using_svn( link ):
+    # a temporary directory
+    import tempfile
+    tempdir = tempfile.mkdtemp()
+    
     import os
     path, filename = os.path.split( link )
     dirname = os.path.split( path )[-1]
     cmds = [
+        # save current path
+        'tempvar_current_path=$PWD',
+        # go into the temp dir
+        'cd %s' % tempdir,
         # check out directory
         'svn co -N %s' % path,
         # cd into the directory and get file
         'cd %s' % dirname,
         'svn up %s' % filename,
-        # mv file up
-        'mv %s ..' % filename,
+        # mv file back
+        'mv %s $tempvar_current_path' % filename,
+        # go back
+        'cd $tempvar_current_path',
         # remove directory
-        'cd ..',
-        'rm -rf %s' % dirname,
+        'rm -rf %s' % tempdir,
         ]
     return cmds
 
