37c356aac1e7d5f28cb2829d50c5bf73bcfd1909
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2008-06-20 14:23:12 +0000

    common/utils/paths/boostpython: check boost python installation by looking for include/boost.python.hpp
    git-svn-id: svn://danse.us/buildInelast/common/utils@275 e9de5ee5-d905-4afb-841f-18304ef63348

--- paths/boostpython.py
+++ paths/boostpython.py
@@ -16,3 +16,14 @@ toolset  = [fromEnvVars,
 from search import search
 paths = search(toolset)
 
+
+#make sure headers are installed
+from InstallationNotFound import InstallationNotFound
+
+# header directory
+include = paths.includes[0]
+# make sure we can find boost/python.hpp
+import os
+python_hpp = os.path.join( include, 'boost', 'python.hpp' )
+if not os.path.exists( python_hpp ):
+    raise InstallationNotFound, name
