04c00a992a881dc6ec3676c29fce669a1ceaa703
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2010-11-03 01:08:52 +0000

    utils/scripts/build.py: this is generic enough to stay here
    git-svn-id: svn://danse.us/buildInelast/common/utils@523 e9de5ee5-d905-4afb-841f-18304ef63348

---
+++ scripts/build.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2007-2010 All Rights Reserved  
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+def build(export_root):
+    import sys, os
+    pwd = os.path.abspath( os.curdir )
+
+    import sys
+    sys.path = [pwd] + sys.path
+
+    from utils.build import build_release, clean_up
+    build_release( pwd, export_root=export_root)
+    return
+
+
+def getsrc():
+    # getsrc if necessary
+    p = 'src'
+    if hasSubdirs(p): return
+    from getsrc import main
+    main()
+    return
+
+
+def hasSubdirs(p):
+    import os
+    entries = os.listdir(p)
+    for e in entries:
+        p1 = os.path.join(p, e)
+        if os.path.isdir(p1): return True
+        continue
+    return False
+
+
+def main():
+    getsrc()
+    import sys, os, shlex
+
+    # find out the "export" directory that user wants
+    from utils.datastore import open
+    build_info = open('build_info')
+    
+    if len(sys.argv) == 2:
+        export_root = sys.argv[1]
+    else:
+        if build_info.get('export_root'):
+            export_root = build_info['export_root']
+        else:
+            export_root = os.path.abspath('EXPORT')
+
+    build_info['export_root'] = export_root
+    del build_info
+
+    # adjust env vars so that <export>/deps is in use
+    deps_root = os.path.join(export_root, 'deps')
+    env = os.environ.copy()
+    env['PATH'] = '%s:%s' % (
+        os.path.join( deps_root, 'bin' ), env['PATH'] )
+    env['LD_LIBRARY_PATH'] = '%s:%s' % (
+        os.path.join( deps_root, 'lib' ), env.get('LD_LIBRARY_PATH') or '' )
+    env['DYLD_LIBRARY_PATH'] = '%s:%s' % (
+        os.path.join( deps_root, 'lib' ), env.get('DYLD_LIBRARY_PATH') or '' )
+    env['PYTHONPATH'] = '%s:%s' % (
+        os.path.join( deps_root, 'python' ), env.get('PYTHONPATH', '') )
+
+    # open a sub process to run the build
+    cmd = 'python -c "from utils.scripts.build import build; build(%r)"' % export_root
+    args = shlex.split(cmd)
+
+    import subprocess
+    p = subprocess.Popen(args, env=env)
+    while p.poll() is None:
+        p.communicate()
+
+
+# version
+__id__ = "$Id$"
+
+# End of file 
