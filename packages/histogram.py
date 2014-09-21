483c0dd18e3697cc89787a26901f91324bfa0c73
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2014-09-21 18:29:05 +0000

    packages/histogram.py: switch to git
    git-svn-id: svn://danse.us/buildInelast/common/utils@648 e9de5ee5-d905-4afb-841f-18304ef63348

--- packages/histogram.py
+++ packages/histogram.py
@@ -4,5 +4,8 @@ deps = ('pythia', 'stdVector','luban', 'nx5')
 
 import repoutils
 reponame = name
-branch = "trunk"
-path, checkoutCmd, updateCmd = repoutils.svn.repoinfo( reponame, branch, name=name )
+# branch = "trunk"
+# path, checkoutCmd, updateCmd = repoutils.svn.repoinfo( reponame, branch, name=name )
+
+branch = "master"
+path, checkoutCmd, updateCmd = repoutils.git.repoinfo( reponame, branch, name=name )
