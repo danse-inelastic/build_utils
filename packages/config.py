2737b5bca088eba27824b66efb992467795348b4
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2015-04-16 15:29:04 +0000

    packages/config.py: switch to git
    git-svn-id: svn://danse.us/buildInelast/common/utils@662 e9de5ee5-d905-4afb-841f-18304ef63348

--- packages/config.py
+++ packages/config.py
@@ -2,8 +2,13 @@ name = 'config'
 deps = []
 
 from utils import repoutils
+
+# svn: old
 reponame = 'ctrl'
 branch = "config/branches/with_doxygen_and_docbook_support"
 path, checkoutCmd, updateCmd = repoutils.svn.repoinfo(reponame, branch, name=name)
 
-
+# git
+reponame = "danse-config"
+branch = "master"
+path, checkoutCmd, updateCmd = repoutils.git.repoinfo( reponame, branch, name=name )
